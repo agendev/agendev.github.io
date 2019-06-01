f0e678a004f38fd40fa95bdcdf862d4a5d351a38
Nicolas White <nickwhite1423@gmail.com>
2019-06-01 14:58:31 -0400

    header fixes

--- app.js
+++ app.js
@@ -3,17 +3,9 @@ $(window).scroll(function() {
 
   if (scroll >= 500) {
     $(".header").addClass("transparent-header");
-    $(".nav-tabs").addClass("transparent");
-    $(".nav-tab").addClass("transparent");
-    $(".nav-tab-link").addClass("transparent");
-    $("#header-logo-link").addClass("gone");
-    $("#header-logo-link-green").removeClass("gone");
+
   } else {
     $(".header").removeClass("transparent-header");
-    $(".nav-tabs").removeClass("transparent");
-    $(".nav-tab").removeClass("transparent");
-    $(".nav-tab-link").removeClass("transparent");
-    $("#header-logo-link").removeClass("gone");
-    $("#header-logo-link-green").addClass("gone");
+
   }
 });
