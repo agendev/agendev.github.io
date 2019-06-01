b4232f092692c4cdf6cc16ba3b279a019bb6eaee
Nicolas White <nickwhite1423@gmail.com>
2019-06-01 00:15:38 -0400

    header stuff

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ $(window).scroll(function() {
   var scroll = $(window).scrollTop();
 
   if (scroll >= 500) {
-    $(".header").addClass("transparent");
+    $(".header").addClass("transparent-header");
     $(".nav-tabs").addClass("transparent");
     $(".nav-tab").addClass("transparent");
     $(".nav-tab-link").addClass("transparent");
     $("#header-logo-link").addClass("gone");
+    $("#header-logo-link-green").removeClass("gone");
   } else {
-    $(".header").removeClass("transparent");
+    $(".header").removeClass("transparent-header");
     $(".nav-tabs").removeClass("transparent");
     $(".nav-tab").removeClass("transparent");
     $(".nav-tab-link").removeClass("transparent");
     $("#header-logo-link").removeClass("gone");
+    $("#header-logo-link-green").addClass("gone");
   }
 });
