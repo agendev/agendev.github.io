f1737edb54d7c0b8efd0d17120a027d6bcd967d7
Nicolas White <nickwhite1423@gmail.com>
2019-06-10 13:03:44 -0400

    fix mobile nav menu links

--- app.js
+++ app.js
@@ -10,14 +10,10 @@ $(window).scroll(function() {
 
 
 function toggleNav() {
-   var x = document.getElementById("myLinks");
+  var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
-    
   } else {
     x.style.display = "block";
-    
   }
-  
-
 }
