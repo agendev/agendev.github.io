321f7f66fcf483041de02e0ec0ca104bde29b0ee
Cole Hastings <cole@seasonedtofu.com>
2019-06-11 20:56:28 -0400

    more mobile home page changes

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
 
-  if (scroll >= 200) {
+  if (scroll >= 150) {
     $(".header").addClass("transparent-header");
   } else {
     $(".header").removeClass("transparent-header");
   }
 });
 
-
+$(document).ready(function(){
+	$('#mobile-home-image').fadeIn(1000);
+	$('#slogan-text').fadeIn(1000);
+	$('#slogan-logo').fadeIn(1000);
+});
 function toggleNav() {
   var box = $("#myLinks");
   box.toggleClass('gone');
- 
+
 }
